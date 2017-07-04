f60b6385e113c1529d311e11713622848a31e435
travis <travis.j.cote@gmail.com>
2017-07-04 09:33:40 -0700

    v2

---
+++ index.php
@@ -0,0 +1,58 @@
+<!doctype html>
+
+<html lang="en" ng-app="myApp">
+		
+	<head>
+	    <meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+	    <title>Twitter Anti-Bot Digest</title>
+	
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">	
+		<link rel="stylesheet" type="text/css" href="../includes/css/bootstrap.min.css">	
+
+		<link rel="stylesheet" type="text/css" href="includes/css/general.css?random=<?php echo uniqid(); ?>">	
+		<link rel="stylesheet" type="text/css" href="includes/css/colors.css?random=<?php echo uniqid(); ?>">	
+		<link rel="stylesheet" type="text/css" href="includes/css/text.css?random=<?php echo uniqid(); ?>">	
+		<link rel="stylesheet" type="text/css" href="includes/css/buttons.css?random=<?php echo uniqid(); ?>">	
+		<link rel="stylesheet" type="text/css" href="includes/css/menus.css?random=<?php echo uniqid(); ?>">	
+		<link rel="stylesheet" type="text/css" href="includes/css/dialogs.css?random=<?php echo uniqid(); ?>">	
+		<link rel="stylesheet" type="text/css" href="includes/css/forms.css?random=<?php echo uniqid(); ?>">	
+		<link rel="stylesheet" type="text/css" href="includes/css/tables.css?random=<?php echo uniqid(); ?>">	
+	  	<link rel="stylesheet" type="text/css" href="includes/css/custom.css?random=<?php echo uniqid(); ?>">
+	
+	  	<!-- common --> 
+	  	<script src="../includes/js/angular.min.js"></script>
+	  	<script src="../includes/js/angular-route.min.js"></script>
+	  	
+	</head>
+	
+	<body>
+		<div class="main" ng-view></div>
+	
+		<!-- javascript -->
+		<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>	
+		<script src="../includes/js/bootstrap-3.3.7/js/bootstrap.min.js"></script>
+		
+	  	<script src="../includes/js/class.class.js?random=<?php echo uniqid(); ?>"></script>
+	  	<script src="../includes/js/class.base.js?random=<?php echo uniqid(); ?>"></script>
+		<script src="../includes/js/util.js?random=<?php echo uniqid(); ?>"></script>
+	
+		<script src="includes/js/class.dialogUI.js?random=<?php echo uniqid(); ?>"></script>
+		<script src="includes/js/class.signupUI.js?random=<?php echo uniqid(); ?>"></script>
+		<script src="includes/js/class.signinUI.js?random=<?php echo uniqid(); ?>"></script>
+		<script src="includes/js/class.addTwitterAccountUI.js?random=<?php echo uniqid(); ?>"></script>
+		
+	  	<script src="includes/js/app.js?random=<?php echo uniqid(); ?>"></script>
+	  	<!-- angular controllers -->
+	  	<script src="includes/js/controllers.js?random=<?php echo uniqid(); ?>"></script>
+
+	  	<!-- javascript controller helpers --> 
+	  	<script src="includes/js/class.redirectController.js?random=<?php echo uniqid(); ?>"></script>
+	  	<script src="includes/js/class.splashController.js?random=<?php echo uniqid(); ?>"></script>
+	  	<script src="includes/js/class.addTwitterAccountController.js?random=<?php echo uniqid(); ?>"></script>
+	  	<script src="includes/js/class.dashboardController.js?random=<?php echo uniqid(); ?>"></script>
+	  	<script src="includes/js/class.tableController.js?random=<?php echo uniqid(); ?>"></script>
+	</body>
+</html>
